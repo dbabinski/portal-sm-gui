12eed0b31ee887f9d2cec96566d8dbd4508c1a95
Damian Babiński <d.babinski94@gmail.com>
2021-11-19 13:57:25 +0100

    Vue.js 3 > Vue.js 2

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,17 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import '@babel/polyfill'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
-createApp(App).use(store).use(router).mount("#app");
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
